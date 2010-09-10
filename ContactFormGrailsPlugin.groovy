39a593d4700a9b7cf4f79374b8b4744b12250b0c
acampbell3000 <acampbell3000@googlemail.com>
2010-09-10 23:00:28 +0100

    Add jquery dependency to plug-in descriptor. In addition, introduce BuildConfig.

--- ContactFormGrailsPlugin.groovy
+++ ContactFormGrailsPlugin.groovy
@@ -1,10 +1,35 @@
+
+/**
+ * Copyright 2010 Anthony Campbell (anthonycampbell.co.uk)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Grails Contact Form Plug-in
+ *
+ * A simple plug-in which provides a ajax driven contact form along with
+ * an image captcha.
+ *
+ * @author Anthony Campbell (anthonycampbell.co.uk)
+ */
 class ContactFormGrailsPlugin {
     // the plugin version
     def version = "1.0.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [ mail: "0.9", jcaptcha: "1.2.1" ]
+    def dependsOn = [ mail: "0.9", jcaptcha: "1.2.1", jquery: "1.4.2.5" ]
     
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
@@ -14,7 +39,7 @@ class ContactFormGrailsPlugin {
 
     // TODO Fill in these fields
     def author = "Anthony Campbell"
-    def authorEmail = "acampbell3000 [[at] googlemail [dot]] com "
+    def authorEmail = "acampbell3000 [[at] mail from google"
     def title = "Grails Contact Form"
     def description = '''
 A simple plug-in which provides an Ajax driven contact form with real time validation and captcha support.
@@ -24,31 +49,4 @@ Ajax forms provided by the "remote-forms" skin available through the skin-loader
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/contact-form"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
